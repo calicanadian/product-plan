07b093ffd94f33090082c530abe44bfd1f983b76
Your name <Your email>
2019-06-21 16:54:09 -0700

    Greenfield app. Install gems forr react, webpacker, haml, jquery, awesome print. install webpacker and react. set root route.

---
+++ app/javascript/components/ProductPlan.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class ProductPlan extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ProductPlan
